9c67c9048b4452c75ffc2bf3bd90cd4deaff35c3
AirOne01 <popgthyrd@gmail.com>
2022-05-13 16:42:08 +0200

    Add variables to init script

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e # exit on error
 
+# config
+script_url="https://github.com/AirOne01/init/raw/main/index.js"
+node_url="https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz"
+archive_path="node-v16.15.0-linux-x64/bin/node"
+archive_strip=2
+
 mkdir ~/.r1_init/bin -p
 work_dir="$HOME/.r1_init"
 bin_dir="$work_dir/bin"
@@ -8,13 +14,9 @@ bin_dir="$work_dir/bin"
 if [ ! "$(command -v node)" ]; then
     echo -n "Node binary not found.\nDownloading (portable version)... " >&2
     if [ "$(command -v curl)" ]; then
-        sh -c "$(curl -so $work_dir/node.tar.xz \
-        https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz)" -- b \
-        "$bin_dir"
+        sh -c "$(curl -so $work_dir/node.tar.xz node_url)" -- b "$bin_dir"
     elif [ "$(command -v wget)" ]; then
-        sh -c "$(wget -qO $work_dir/node.tar.xz \
-        https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.xz)" -- b \
-        "$bin_dir"
+        sh -c "$(wget -qO $work_dir/node.tar.xz node_url)" -- b "$bin_dir"
     else
         echo "\nTo run the script, you must have curl or wget installed." >&2
         exit 1
@@ -24,7 +26,8 @@ if [ ! "$(command -v node)" ]; then
     # tar -Jxvf "$work_dir/node.tar.xz" \
     # "node-v16.15.0-linux-x64/bin/node -C $bin_dir --strip-components=2"
     cd $bin_dir
-    tar -Jxf "$work_dir/node.tar.xz" "node-v16.15.0-linux-x64/bin/node" --strip-components=2
+    tar -Jxf "$work_dir/node.tar.xz" "$archive_path" \
+    --strip-components=$archive_strip
     rm $work_dir/node.tar.xz
     chmod +x $bin_dir/node
     echo "ok."
@@ -34,13 +37,9 @@ fi
 
 echo -n "Getting script... " >&2
 if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -so $work_dir/index.js \
-    https://github.com/AirOne01/init/raw/main/index.js)" -- b \
-    "$work_dir"
+    sh -c "$(curl -so $work_dir/index.js $script_url)" -- b "$work_dir"
 elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO $work_dir/index.js \
-    https://github.com/AirOne01/init/raw/main/index.js)" -- b \
-    "$work_dir"
+    sh -c "$(wget -qO $work_dir/index.js $script_url)" -- b "$work_dir"
 else
     echo "To run the script, you must have curl or wget installed." >&2
     exit 1
